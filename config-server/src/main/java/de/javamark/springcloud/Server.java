dcc94dfee71ca03f5e22dac5f7c18c5ea80335a8
Mark Goericke <mark@javamark.de>
2018-06-03 14:51:03 +0200

    initial commit

---
+++ config-server/src/main/java/de/javamark/springcloud/Server.java
@@ -0,0 +1,20 @@
+package de.javamark.springcloud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Server {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[]args) {
+
+        LOGGER.info("server startup");
+        SpringApplication.run(Server.class, args);
+    }
+}
